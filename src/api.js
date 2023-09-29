8fbc7d5c97003799ba172f1d544f5028015eb31f
Suayb Kardas <kardassuayb@gmail.com>
2023-09-29 15:28:45 +0300

    App created and city data fetching from geoDB API

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6f533c5decmshbf1ee8741ba4b4ep12edc2jsn425dabbab5fd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
